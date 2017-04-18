e25686c73a92a3fec32c75522a1e2e6f0df21cef
Patricio Reyna Almandos <preyna@ekumenlabs.com>
2017-04-18 17:13:41 -0300

    added script user documentation

--- docker/run_s3bucket_container.bash
+++ docker/run_s3bucket_container.bash
@@ -12,12 +12,27 @@
 # --- $HOME/.passwd-s3fs: contains AWS keys in this format -> public:private
 # --- $HOME/.bucketname-s3fs: contains the bucket name to use in S3
 
+if [ $# -lt 2 ] || [ $# -gt 3 ]; then
+  echo "This script is used to upload a host path to S3."
+  echo "The script expects 3 arguments:"
+  echo "--- 1) The image name."
+  echo "--- 2) The host path that will be uploaded."
+  echo "--- 3) An optional command to execute in the running container. E.g. /bin/bash "
+  echo "Example command line:"
+  echo "$ ./run_s3bucket_container.bash s3bucket HOME/myfolder/"
+  echo ""
+  echo "The script also assumes there are 2 files in the host's user home:"
+  echo "--- HOME/.passwd-s3fs: contains AWS keys in this format -> public:private"
+  echo "--- HOME/.bucketname-s3fs: contains the bucket name to use in S3"
+  exit 1
+fi
+
 IMAGE_NAME=$1
 HOST_PATH=$2
 COMMAND=$3
 
-# --privileged is needed if we want to use s3fs mounts
-sudo docker run --rm -ti \
+# note: --privileged is needed if we want to use s3fs mounts
+docker run --rm \
   --device=/dev/fuse \
   --privileged \
   -v "/etc/localtime:/etc/localtime:ro" \
